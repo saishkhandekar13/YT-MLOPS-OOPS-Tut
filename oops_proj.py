d1cb0c18e1eabd2795923226039bde87c1024b3a
Saish <saishkhandekar13@gmail.com>
2026-01-11 19:28:36 +0530

    Covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -11,15 +11,17 @@ class chatbook:
                            2. Press 2 to signin
                            3. Press 3 to write a post
                            4. Press 4 to message the friend
-                           5. Press any other key to exit""")
+                           5. Press any other key to exit
+                           
+                           -> """)
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input == "4":
-            pass
+            self.sendmsg()
         else:
             exit() 
 
@@ -44,7 +46,25 @@ class chatbook:
             else:
                 print("Please input correct credentials..")
         print("\n")
-        self.menu()            
+        self.menu()
 
+    def my_post(self):
+        if self.loggedin==True:
+            txt = input("Enter your messsage here -> ")
+            print(f"Following content has been posted -> {txt}")          
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()  
+
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            frnd = input("Whom to send the msg ->     ")
+            print(f"Your message has been sent to {frnd}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()        
 obj = chatbook()
 
